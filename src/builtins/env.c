fbb69aa3c0eab7694b80106f319b8cb84b849cbb
Yves Gille <ygille@student.42lyon.fr>
2025-01-29 17:25:37 +0100

    Implémentation de env

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: ygille <ygille@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 15:14:29 by ygille            #+#    #+#             */
-/*   Updated: 2025/01/29 15:14:35 by ygille           ###   ########.fr       */
+/*   Updated: 2025/01/29 17:24:46 by ygille           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 void	func_env(t_minishell *minishell)
 {
-	(void)minishell;
+	int	i;
+
+	i = 0;
+	while (minishell->envp[i] != NULL)
+		ft_putendl_fd(minishell->envp[i++], 1);
+	minishell->last_return_value = 0;
 }
