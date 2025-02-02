7e4f0f30f1276d92dc77f2f24879dce431703e03
Yves Gille <ygille@student.42lyon.fr>
2025-02-02 19:37:54 +0100

    Variable inutile

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: ygille <ygille@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 15:14:29 by ygille            #+#    #+#             */
-/*   Updated: 2025/01/30 16:23:46 by ygille           ###   ########.fr       */
+/*   Updated: 2025/02/02 19:37:26 by ygille           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,8 @@
 */
 int	func_env(t_minishell *minishell)
 {
-	int		i;
 	t_mlist	*lst;
 
-	i = 0;
 	lst = minishell->env;
 	while (lst != NULL)
 	{
