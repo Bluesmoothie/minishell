c8dfb4a7073b6fd2945e744c11e10b9b0fcf30b7
Yves Gille <ygille@student.42lyon.fr>
2025-02-10 19:23:50 +0100

    Ajout de tests

--- tests/tests.sh
+++ tests/tests.sh
@@ -9,4 +9,43 @@ export E=EE
 echo $EE
 unset EE
 echo $EE
-wefhwejhfbwejh
+echo Hello, World!
+ls -la
+pwd
+cd ..
+env
+echo test > file.txt
+cat < file.txt
+ls -l >> output.txt
+cat file1.txt > file2.txt
+ls | grep .txt
+cat file.txt | sort | uniq
+echo Hello | tr a-z A-Z
+echo $HOME
+echo $PATH
+export TEST=value
+echo $TEST
+unset TEST
+echo "Double quotes $HOME"
+echo 'Single quotes $HOME'
+echo Backslash $HOME
+echo "Mixed 'quotes'"
+echo Hello ; ls ; pwd
+cd /tmp && touch testfile && ls -l testfile
+non_existent_command
+ls non_existent_file
+cd /root
+sleep 10
+cat
+echo *
+echo ~
+exit
+cd -
+cd ~
+VAR=test
+echo $VAR
+unset VAR
+echo $?
+echo "Bonjour ; ls"
+echo Bonjour > test\ 1
+cat /dev/random | head -n 1 | cat -e
