74ea8f9f7aabba8691fb58d7735c1042380b75a3
Yves Gille <ygille@student.42lyon.fr>
2025-02-13 13:22:25 +0100

    Protection de export

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: ygille <ygille@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 15:14:06 by ygille            #+#    #+#             */
-/*   Updated: 2025/02/11 16:55:56 by ygille           ###   ########.fr       */
+/*   Updated: 2025/02/13 13:17:11 by ygille           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,14 +32,14 @@ int	func_export(t_minishell *minishell, char **args, int fd)
 		name = extract_name(args[1]);
 		content = extract_content(args[1]);
 	}
-	if (!ft_mlstsearch(minishell->env, name))
+	if (!ft_mlstsearch(minishell->env, name) && name[0] != '\0')
 	{
 		new = ft_mlstcreate(name, content);
 		if (new == NULL)
 			free_exit(minishell, args, E_MALLOCFAIL);
 		minishell->env = ft_mlstadd_front(minishell->env, new);
 	}
-	else
+	else if (name[0] != '\0')
 		replace_env(minishell, name, content);
 	return (0);
 }
