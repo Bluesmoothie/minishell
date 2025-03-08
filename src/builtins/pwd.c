cdc19f7b6df68c296647000f0ca24d2c227e8660
Yves Gille <ygille@student.42lyon.fr>
2025-03-08 12:31:46 +0100

    Fix : error when pwd on deleted dir

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sithomas <sithomas@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: ygille <ygille@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 15:13:56 by ygille            #+#    #+#             */
-/*   Updated: 2025/02/07 13:08:57 by sithomas         ###   ########.fr       */
+/*   Updated: 2025/03/08 12:30:13 by ygille           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@
 */
 int	func_pwd(t_minishell *minishell, int fd)
 {
-	ft_putendl_fd(minishell->pwd, fd);
+	if (minishell->pwd)
+		ft_putendl_fd(minishell->pwd, fd);
+	else
+		display_error("pwd", " : Don't know where I am :(", NULL);
 	return (0);
 }
