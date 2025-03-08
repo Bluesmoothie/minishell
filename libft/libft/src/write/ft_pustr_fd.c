625d94e26a0ff84808e9cda893a283f8f404b7bc
Yves Gille <ygille@student.42lyon.fr>
2025-03-08 12:39:18 +0100

    Protect ft_putstr by verifying null on string

--- libft/libft/src/write/ft_pustr_fd.c
+++ libft/libft/src/write/ft_pustr_fd.c
@@ -6,7 +6,7 @@
 /*   By: ygille <ygille@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 18:28:34 by ygille            #+#    #+#             */
-/*   Updated: 2025/02/07 17:59:02 by ygille           ###   ########.fr       */
+/*   Updated: 2025/03/08 12:38:30 by ygille           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	ft_putstr_fd(char *s, int fd)
 	size_t	i;
 
 	i = 0;
-	while (s[i])
-		write (fd, &s[i++], 1);
+	if (s)
+		while (s[i])
+			write (fd, &s[i++], 1);
 }
