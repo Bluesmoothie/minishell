c80daf97980ba5535515d3039472b426f03ed00e
Yves Gille <ygille@student.42lyon.fr>
2025-03-08 12:35:07 +0100

    Fix + personnalized message

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: ygille <ygille@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 15:13:56 by ygille            #+#    #+#             */
-/*   Updated: 2025/03/08 12:30:13 by ygille           ###   ########.fr       */
+/*   Updated: 2025/03/08 12:34:05 by ygille           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int	func_pwd(t_minishell *minishell, int fd)
 	if (minishell->pwd)
 		ft_putendl_fd(minishell->pwd, fd);
 	else
-		display_error("pwd", " : Don't know where I am :(", NULL);
+		display_error("pwd", " : Don't know where I am :(",
+			" Did you removed the current directory ?");
 	return (0);
 }
